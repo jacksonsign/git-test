bdbd36f592d3d5712eec753deb4cac85570d37a1
jackson <18319276155@163.com>
2023-12-24 17:45:40 +0800

    init project

---
+++ src/main/java/com/git/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+/**
+ * @Author: jackson
+ * @Date: 2023/12/24 17:32
+ * @Description: TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest...");
+    }
+}
