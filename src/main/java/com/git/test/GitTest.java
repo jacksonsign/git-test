64b4f45872213bd38aaf3f044953f249e03d3f4e
jackson <18319276155@163.com>
2023-12-24 18:19:53 +0800

    ls update project

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -8,5 +8,15 @@ package com.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest...");
+        ls();
+        ls2();
+    }
+
+    private static void ls2() {
+        System.out.println("ls2...");
+    }
+
+    private static void ls() {
+        System.out.println("ls...");
     }
 }
